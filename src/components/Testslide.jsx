98df6ef6f9eec6658fe7f5f41472048d897aa15d
Marius YVART <mariusyvt@gmail.com>
2024-06-26 23:04:16 +0200

    Update: Mise à jour du carrousel

--- src/components/Testslide.jsx
+++ src/components/Testslide.jsx
@@ -169,7 +169,25 @@
 
 import { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
+
 import logo from "./../assets/img/logo.webp";
+import beeautop from "./../assets/img/beeautop.webp";
+import beez2be from "./../assets/img/beez2be.webp";
+import foodtruck from "./../assets/img/foodtruck.webp";
+import angouloriente from "./../assets/img/angouloriente.webp";
+// import Tailwindcss from "./../assets/img/tailwindcss.svg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faReact,
+  faJs,
+  faSass,
+  faWordpress,
+  faPhp,
+  faHtml5,
+  faCss3Alt,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 
 function Testslide() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -181,18 +199,24 @@ function Testslide() {
     {
       id: 1,
       titre: "01",
-      nom: "Beez2Be",
+      nom: "Angoul'Oriente",
       content:
-        "Dans le cadre d'un projet de 2ème année en groupe, nous avons développé un site web fictif dédié à un food truck. Le site a été conçu en utilisant les langages HTML, CSS, PHP, et Javascript. Mon rôle spécifique a été la création de la page de commande. Cette section du site offre la possibilité de réserver un repas pour une date et une heure spécifiques. Pour plus d'informations, vous pouvez consulter le code sur mon GitHub.",
-      img: logo,
+        "Cette application vous fait vivre une course d'orientation interactive. Pour terminer la course, vous devez passer par 8 points définis dans la ville d'Angoulême. Ces points sont visibles sur la carte ainsi que votre position en temps réel. Attention ! Il est important de respecter l'ordre des points en commençant par le point n°1 et en finissant par le point n°8. À chaque point, vous trouverez un QR code que vous pourrez scanner en cliquant sur le ping correspondant au point sur la carte. Une fois le QR scanné, une lettre vous sera donnée. Récupérez les 8 lettres pour former un mot. À noter que la dernière balise n'est pas une lettre mais un nombre qui représente la clé d'un chiffrement de César afin de ne pas pouvoir deviner le mot avant d'avoir fait toutes les balises (plus d'infos sur le chiffrement de César : https://e-nsi.forge.aeif.fr/pratique/N1/421-code_cesar/sujet/). Une fois le mot final trouvé et déchiffré, il vous suffit de l'écrire dans le champ prévu à cet effet pour terminer la course et arrêter le chrono !",
+      img: angouloriente,
+      technologies: ["React", "Sass"],
+      url: "https://angouloriente.netlify.app/",
+      annee: "2024",
     },
     {
       id: 2,
       titre: "02",
-      nom: "Angouloriente",
+      nom: "Beez2Be",
       content:
-        "Cette application vous fait vivre une course d'orientation interactive. Pour terminer la course, vous devez passer par 8 points définis dans la ville d'Angoulême. Ces points sont visibles sur la carte ainsi que votre position en temps réel. Attention ! Il est important de respecter l'ordre des points en commençant par le point n°1 et en finissant par le point n°8. À chaque point, vous trouverez un QR code que vous pourrez scanner en cliquant sur le ping correspondant au point sur la carte. Une fois le QR scanné, une lettre vous sera donnée. Récupérez les 8 lettres pour former un mot. À noter que la dernière balise n'est pas une lettre mais un nombre qui représente la clé d'un chiffrement de César afin de ne pas pouvoir deviner le mot avant d'avoir fait toutes les balises (plus d'infos sur le chiffrement de César : https://e-nsi.forge.aeif.fr/pratique/N1/421-code_cesar/sujet/). Une fois le mot final trouvé et déchiffré, il vous suffit de l'écrire dans le champ prévu à cet effet pour terminer la course et arrêter le chrono !",
-      img: logo,
+        "Dans le cadre d'un projet de 2ème année en groupe, nous avons développé un site web fictif dédié à un food truck. Le site a été conçu en utilisant les langages HTML, CSS, PHP, et Javascript. Mon rôle spécifique a été la création de la page de commande. Cette section du site offre la possibilité de réserver un repas pour une date et une heure spécifiques. Pour plus d'informations, vous pouvez consulter le code sur mon GitHub.",
+      img: beez2be,
+      technologies: ["React", "MongoDb"],
+      url: "https://example.com/angouloriente",
+      annee: "2024",
     },
     {
       id: 3,
@@ -200,24 +224,35 @@ function Testslide() {
       nom: "Foodtruck",
       content:
         "Cette application vous fait vivre une course d'orientation interactive. Pour terminer la course, vous devez passer par 8 points définis dans la ville d'Angoulême. Ces points sont visibles sur la carte ainsi que votre position en temps réel. Attention ! Il est important de respecter l'ordre des points en commençant par le point n°1 et en finissant par le point n°8. À chaque point, vous trouverez un QR code que vous pourrez scanner en cliquant sur le ping correspondant au point sur la carte.",
-      img: logo,
+      img: foodtruck,
+      technologies: ["PHP", "Html", "Css", "JavaScript"],
+      url: "https://rtiphonet.fr/foodtruck/accueil.php",
+      annee: "2024",
     },
     {
       id: 4,
       titre: "04",
-      nom: "BOnjour",
+      nom: "BeeAuTop",
       content:
         "Cette application vous fait vivre une course d'orientation interactive. Pour terminer la course, vous devez passer par 8 points définis dans la ville d'Angoulême. Ces points sont visibles sur la carte ainsi que votre position en temps réel. Attention ! Il est important de respecter l'ordre des points en commençant par le point n°1 et en finissant par le point n°8. À chaque point, vous trouverez un QR code que vous pourrez scanner en cliquant sur le ping correspondant au point sur la carte.",
-      img: logo,
+      img: beeautop,
+      technologies: ["PHP", "Html", "Css", "JavaScript"],
+      url: "https://kbelin.alwaysdata.net/beeautop/",
+      annee: "2023",
     },
-    {
-      id: 5,
-      titre: "05",
-      nom: "Aurevoir",
-      content:
-        "Cette application vous fait vivre une course d'orientation interactive. Pour terminer la course, vous devez passer par 8 points définis dans la ville d'Angoulême. Ces points sont visibles sur la carte ainsi que votre position en temps réel. Attention ! Il est important de respecter l'ordre des points en commençant par le point n°1 et en finissant par le point n°8. À chaque point, vous trouverez un QR code que vous pourrez scanner en cliquant sur le ping correspondant au point sur la carte.",
-      img: logo,
-    },
+  ];
+
+  const technologies = [
+    { name: "React", icon: faReact },
+    { name: "JavaScript", icon: faJs },
+    { name: "Sass", icon: faSass },
+    { name: "WordPress", icon: faWordpress },
+    { name: "MongoDb", icon: faDatabase },
+    { name: "PHP", icon: faPhp },
+    { name: "Html", icon: faHtml5 },
+    { name: "Css", icon: faCss3Alt },
+
+    // { name: "Tailwindcss", icon: Tailwindcss },
   ];
 
   const handleNext = () => {
@@ -235,7 +270,7 @@ function Testslide() {
     tl.to(titreRef.current.children[1], {
       x: direction === "next" ? "-100%" : "100%",
       opacity: 1,
-      duration: 0.4,
+      duration: 0.3,
       onComplete: () => {
         setCurrentSlide(nextSlide);
         tl.set(titreRef.current.children[1], {
@@ -244,7 +279,7 @@ function Testslide() {
         }); // Réinitialiser pour la prochaine animation
         tl.to(titreRef.current.children[1], {
           x: "0%",
-          duration: 0.4,
+          duration: 0.3,
         });
       },
     });
@@ -281,15 +316,40 @@ function Testslide() {
                     </span>
                   ))}
                 </h1>
-                <h1 className="carrousel__num-nom" ref={nomRef}>
-                  {slides[currentSlide].nom}
-                </h1>
+                <div>
+                  <h1 className="carrousel__num-nom" ref={nomRef}>
+                    {slides[currentSlide].nom}
+                  </h1>
+                  <div className="btn__container">
+                    {slides[currentSlide].technologies.map((tech, index) => (
+                      <div key={index} className="projet__btn">
+                        <FontAwesomeIcon
+                          icon={technologies.find((t) => t.name === tech).icon}
+                          className="projet__icon"
+                        />
+                        <p className="projet__text">{tech}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
               <div className="carrousel__content">
                 <p className="carrousel__content-p" ref={contentRef}>
                   {slides[currentSlide].content}
                 </p>
-                <img src={slides[currentSlide].img} alt="" />
+                <div className="projet__containerimg">
+                  <a
+                    href={slides[currentSlide].url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      className="projet__img"
+                      src={slides[currentSlide].img}
+                      alt=""
+                    />
+                  </a>
+                </div>
               </div>
               <div className="carrousel__arrow">
                 <svg
